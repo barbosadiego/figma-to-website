98a576191932ef20d146d9c681131f499ed90491
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-19 10:52:52 -0300

    update js to type module

--- ttoil/js/main.js
+++ ttoil/js/main.js
@@ -1,33 +1,5 @@
-const menuBtn = document.querySelector('[data-menu-btn]');
-const closeBtn = document.querySelector('[data-close]');
-const logo = document.querySelector('[data-logo] img');
-const nav = document.querySelector('[data-nav]');
-const menuEl = document.querySelector('[data-menu]');
-const header = document.querySelector('header');
+import menuMobile from "./menu.js";
+import slide from './slide.js';
 
-// active mobile menu
-function handleMenu() {
-  let state = this.getAttribute('aria-expanded') === 'false';
-  this.setAttribute('aria-expanded', state);
-
-  nav.classList.toggle('active');
-  header.classList.toggle('active');
-
-  if(nav.classList.contains('active')){
-    this.setAttribute('aria-label', 'close menu');
-    logo.src = '/ttoil/img/logo-dark.svg';
-    closeBtn.classList.remove('hidde');
-    menuBtn.classList.add('hidde');
-    menuEl.classList.remove('hidde');
-  } else {
-    this.setAttribute('aria-label', 'open menu');
-    logo.src = '/ttoil/img/ttoil-logo.png';
-    closeBtn.classList.add('hidde');
-    menuBtn.classList.remove('hidde');
-    menuEl.classList.add('hidde');
-  }
-}
-
-// events
-menuBtn.addEventListener('click', handleMenu);
-closeBtn.addEventListener('click', handleMenu);
+menuMobile();
+slide();
