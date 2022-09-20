2d35d8cffc1e06262928e66c4ffa07b5bf769a1c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-20 11:42:20 -0300

    add touch events

--- ttoil/js/slide.js
+++ ttoil/js/slide.js
@@ -5,7 +5,7 @@ export default function slide() {
     const left = document.querySelector('[data-slide="left"]');
     const right = document.querySelector('[data-slide="right"]');
     let slideItems = document.querySelectorAll('[data-slide="item"]');
-    let positions = {startX: 0, totalMoviment: 0}
+    let positions = { startX: 0, totalMoviment: 0 };
 
     //show previous element of slide
     function previous() {
@@ -24,35 +24,48 @@ export default function slide() {
     //pointer events
     function handleEvent(e) {
       if (e.type === 'touchstart') {
-        e.preventDefault();
-        console.log('touch');
+        e.preventDefault(e);
+        onStart(e);
         return;
       }
-      this.addEventListener('mousedown', onMouseDown);
+      this.addEventListener('mousedown', onStart);
     }
 
-    function onMouseDown(e) {
-      positions.startX = e.x
+    function onStart(e) {
+      let event = e.type;
       slidesContainer.style.cursor = 'grabbing';
-      slideItems.forEach(slide => slide.style.transition = 'none')
-      this.addEventListener('mousemove', onMouseMove)
-      this.addEventListener('mouseup', onMouseUp)
+      slideItems.forEach((slide) => (slide.style.transition = 'none'));
+      if (event === 'touchstart') {
+        positions.startX = e.touches[0].clientX;
+        slidesContainer.addEventListener('touchmove', onMove);
+        slidesContainer.addEventListener('touchend', onEnd);
+      } else {
+        positions.startX = e.x;
+        this.addEventListener('mousemove', onMove);
+        this.addEventListener('mouseup', onEnd);
+      }
     }
 
-    function onMouseMove(e){
-      positions.totalMoviment = positions.startX - e.x
-      slideItems[1].style.transform = `translate3d(${-positions.totalMoviment}px, 0px, 0px)`
+    function onMove(e) {
+      let event = e.type;
+      if (event === 'touchmove') {
+        positions.totalMoviment = positions.startX - e.touches[0].clientX
+      } else {
+        positions.totalMoviment = positions.startX - e.x;
+      }
+      slideItems[1].style.transform = `translate3d(${-positions.totalMoviment}px, 0px, 0px)`;
     }
-    
-    function onMouseUp(){
-      if(positions.totalMoviment > 10) previous()
-      else if (positions.totalMoviment < 10) next()
+
+    function onEnd() {
+      if (positions.totalMoviment > 10) previous();
+      else if (positions.totalMoviment < 10) next();
       slidesContainer.style.cursor = 'grab';
-      this.removeEventListener('mousemove', onMouseMove)
+      this.removeEventListener('mousemove', onMove);
+      this.removeEventListener('touchmove', onMove);
       slideItems.forEach((slide) => {
         slide.style.transform = ``;
-        slide.style.transition = '.3s'
-      })
+        slide.style.transition = '.3s';
+      });
     }
 
     //EVENTS
