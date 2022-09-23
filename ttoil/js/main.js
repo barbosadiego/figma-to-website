b7b50140a26d47dfde850cc76d78dfb4be0bf470
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-23 10:27:55 -0300

    add smooth scroll behavior

--- ttoil/js/main.js
+++ ttoil/js/main.js
@@ -1,7 +1,9 @@
 import menuMobile from "./menu.js";
 import slide from './slide.js';
 import windowDesktop from './window-desktop.js';
+import scroll from './scroll.js';
 
 menuMobile();
 slide();
 windowDesktop();
+scroll();
---
+++ ttoil/js/scroll.js
@@ -0,0 +1,17 @@
+export default function scroll(){
+  const links = document.querySelectorAll('.nav a[href^="#"]');
+
+  links.forEach((link) => link.addEventListener('click', handleClick))
+
+  function handleClick(e){
+    e.preventDefault();
+    const section = e.target.getAttribute('href');
+    const pageSection = document.querySelector(section);
+    const pageToTop = pageSection.offsetTop;
+
+    window.scrollTo({
+      top: pageToTop,
+      behavior: 'smooth',
+    })
+  }
+}
