5db95d5bc8dee74ab31ad2ba653676312558fa5f
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-23 10:14:29 -0300

    update mobile menu and add html sections ids

--- ttoil/js/menu.js
+++ ttoil/js/menu.js
@@ -1,4 +1,6 @@
 export default function menuMobile() {
+  const windowWidth = window.matchMedia('(max-width: 764px)');
+
   const menuBtn = document.querySelector('[data-menu-btn]');
   const closeBtn = document.querySelector('[data-close]');
   const logo = document.querySelector('[data-logo] img');
@@ -7,31 +9,47 @@ export default function menuMobile() {
   const header = document.querySelector('header');
   const links = document.querySelectorAll('[data-menu] a[href^="#"]');
 
-  // active mobile menu
-  function handleMenu() {
-    let state = this.getAttribute('aria-expanded') === 'false';
-    this.setAttribute('aria-expanded', state);
+  function applyMatch() {
+    if (windowWidth.matches) {
 
-    nav.classList.toggle('active');
-    header.classList.toggle('active');
+      // active mobile menu
+      function handleMenu() {
+        let state = this.getAttribute('aria-expanded') === 'false';
+        this.setAttribute('aria-expanded', state);
 
-    if (nav.classList.contains('active')) {
-      this.setAttribute('aria-label', 'close menu');
-      logo.src = '/ttoil/img/logo-dark.svg';
-      closeBtn.classList.remove('hidde');
-      menuBtn.classList.add('hidde');
-      menuEl.style.display = 'flex';
-    } else {
-      this.setAttribute('aria-label', 'open menu');
-      logo.src = '/ttoil/img/ttoil-logo.png';
-      closeBtn.classList.add('hidde');
-      menuBtn.classList.remove('hidde');
-      menuEl.style.display = 'none';
+        nav.classList.toggle('active');
+        header.classList.toggle('active');
+
+        if (nav.classList.contains('active')) {
+          this.setAttribute('aria-label', 'close menu');
+          logo.src = '/ttoil/img/logo-dark.svg';
+          closeBtn.classList.remove('hidde');
+          menuBtn.classList.add('hidde');
+          menuEl.style.display = 'flex';
+        } else {
+          this.setAttribute('aria-label', 'open menu');
+          logo.src = '/ttoil/img/ttoil-logo.png';
+          closeBtn.classList.add('hidde');
+          menuBtn.classList.remove('hidde');
+          menuEl.style.display = 'none';
+        }
+      }
+
+      // events
+      menuBtn.addEventListener('click', handleMenu);
+      closeBtn.addEventListener('click', handleMenu);
+      links.forEach((link) => link.addEventListener('click', handleMenu));
     }
   }
 
-  // events
-  menuBtn.addEventListener('click', handleMenu);
-  closeBtn.addEventListener('click', handleMenu);
-  links.forEach(link => link.addEventListener('click', handleMenu))
+  function debounce(func, wait){
+    let timeout;
+    return function (){
+      clearTimeout(timeout);
+      timeout = setTimeout(func, wait);
+    }
+  }
+
+  window.addEventListener('resize', debounce(applyMatch, 50))
+  applyMatch();
 }
